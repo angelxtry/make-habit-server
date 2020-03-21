46548e9b99036ec1d9bf7ed3cd7ca939ad8cf8c4
LeeSuHo <angelxtry@gmail.com>
2020-03-21 23:43:05 +0900

    User entity 추가

---
+++ src/entities/User.ts
@@ -0,0 +1,77 @@
+import * as bcrypt from 'bcrypt';
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+} from 'typeorm';
+import { IsEmail } from 'class-validator';
+
+const BCRYPT_ROUND = 10;
+
+export enum SnsDiv {
+  GOOGLE = 'GOOGLE',
+  FACEBOOK = 'FACEBOOK',
+}
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text', unique: true })
+  @IsEmail()
+  email: string | null;
+
+  @Column({ type: 'boolean', default: false })
+  verifiedEmail: boolean;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @Column({ type: 'int', nullable: true })
+  age: number;
+
+  @Column({ type: 'text', nullable: true })
+  password: string;
+
+  @Column({ type: 'text', nullable: true })
+  snsId: string;
+
+  @Column({ type: 'enum', enum: SnsDiv, nullable: true })
+  snsDiv: SnsDiv;
+
+  @Column({ type: 'text', nullable: true })
+  profilePhoto: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUND);
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if (this.password) {
+      console.log(this.password);
+      const hashedPassword = await this.hashPassword(this.password);
+      console.log(hashedPassword);
+      this.password = hashedPassword;
+    }
+  }
+
+  public comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+}
+
+export default User;
