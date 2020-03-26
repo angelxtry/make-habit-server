1a1c02a6dabdd83382d938c59fba84e0212a5517
LeeSuHo <angelxtry@gmail.com>
2020-03-26 17:08:03 +0900

    authMiddleware 추가.
    header의 token을 확인 -> token 복호화 -> user 조회 -> req.user 추가.
    express.Request에 user property 추가.

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,35 @@ import * as cors from 'cors';
 import * as helmet from 'helmet';
 import * as logger from 'morgan';
 import schema from './schema';
+import decodeJWT from './utils/decodeJWT';
 
 const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(logger('dev'));
-const server = new ApolloServer({ schema });
+
+const authMiddleware = async (
+  req: express.Request,
+  _: unknown,
+  next: express.NextFunction,
+): Promise<void> => {
+  const token = req.get('H-TOKEN');
+  if (token) {
+    const user = await decodeJWT(token);
+    if (user) {
+      req.user = user;
+    } else {
+      req.user = undefined;
+    }
+  }
+  next();
+};
+app.use(authMiddleware);
+
+const server = new ApolloServer({
+  schema,
+  context: ({ req }: { req: express.Request }) => ({ req }),
+});
 server.applyMiddleware({ app });
 
 export default app;
