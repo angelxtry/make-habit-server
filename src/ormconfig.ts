d9fb1b64019520a132748821065796d04b725bab
LeeSuHo <angelxtry@gmail.com>
2020-03-27 21:06:36 +0900

    ormconfig 내용 수정
    dotenv 설정 코드 추가.
    entities, NODE_ENV에 따라 분기 처리.

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,11 +1,18 @@
 import { ConnectionOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectionOption: ConnectionOptions = {
   type: 'postgres',
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: ['dist/entities/**/*.*'],
+  // dropSchema: true,
+  entities:
+    process.env.NODE_ENV === 'production'
+      ? ['dist/entities/**/*.*']
+      : ['src/entities/**/*.*'],
   host: process.env.DB,
   port: 5432,
   username: process.env.DB_USERNAME,
